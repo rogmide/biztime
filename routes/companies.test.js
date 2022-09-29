0853c3ffc84b3731e6a836a8f4c9f545a56cba9d
Roger Delgado <rogmide@gmail.com>
2022-09-29 09:27:09 -0400

    adding more test to invoice

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -3,21 +3,80 @@ const request = require("supertest");
 const app = require("../app");
 const db = require("../db");
 
-let testUser;
+let testComp;
 beforeEach(async () => {
   const result = await db.query(
-    `insert into users
+    `insert into companies
      (code, name, description) 
      values ('apple', 'Apple Inc.', 'MacBook Pro') 
-     returning id, name, type`
+     returning code, name, description`
   );
-  testUser = result.rows[0];
+  testComp = result.rows[0];
 });
 
 afterEach(async () => {
-  await db.query("delete from users");
+  await db.query("delete from companies");
 });
 
 afterAll(async () => {
   await db.end();
+});
+
+describe("Get /companies", () => {
+  test("Get a list with one company", async () => {
+    const res = await request(app).get("/companies");
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual({ companies: [testComp] });
+  });
+});
+
+describe("Get /companies/:code", () => {
+  test("Get a single company", async () => {
+    const res = await request(app).get(`/companies/${testComp.code}`);
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual({ company: testComp, invoices: [] });
+  });
+
+  describe("Get /companies/:code", () => {
+    test("Responds with 404 for invalid code", async () => {
+      const res = await request(app).get(`/companies/notHere`);
+      expect(res.statusCode).toBe(404);
+    });
+  });
+});
+
+describe("Post /companies/", () => {
+  test("Create a company", async () => {
+    const res = await request(app)
+      .post(`/companies`)
+      .send({ code: "TestCode", name: "TestName", description: "TestDes" });
+    expect(res.statusCode).toBe(201);
+    expect(res.body).toEqual({
+      company: { code: "TestCode", name: "TestName", description: "TestDes" },
+    });
+  });
+});
+
+describe("Patch /companies/", () => {
+  test("Update a single company", async () => {
+    const res = await request(app)
+      .patch(`/companies/${testComp.code}`)
+      .send({ code: "apple", name: "TestName", description: "TestDes" });
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual({
+      company: { code: "apple", name: "TestName", description: "TestDes" },
+    });
+  });
+  test("Not company found", async () => {
+    const res = await request(app).patch(`/companies/NotACompany`);
+    expect(res.statusCode).toBe(404);
+  });
+});
+
+describe("Delete /company/", () => {
+  test("Delete a single company", async () => {
+    const res = await request(app).delete(`/companies/${testComp.code}`);
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual({ msg: "Deleted!" });
+  });
 });
